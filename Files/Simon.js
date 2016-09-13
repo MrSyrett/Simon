cb976453ae8698e13e6d7a3546cad98cbdeb8a8f
Michael <mrsyrett@gmail.com>
2016-09-13 22:25:30 +0000

    160913.1 building game button functionality - incomplete

--- Files/Simon.js
+++ Files/Simon.js
@@ -1,5 +1,68 @@
 // JavaScript File
 $(document).ready(function(){
     
+    var level = 1;
+    var gameSeq = [];
+    var playerSeq = [];
+    var isRunning = false;
+    var playerTurn = false;
+    
+    
+    
+    function startGame(){
+        resetGame();
+        getNextColor();
+        displaySeq();
+    }
+    
+    function resetGame(){
+        level = 1;
+        gameSeq = [];
+        playerSeq = [];
+        isRunning = true;
+    }
+    
+    function nextColor(){
+    return Math.floor(Math.random() * 4);
+    }
+    
+    function getNextColor(){
+       gameSeq.push(nextColor()); 
+    }
+    
+    function displayButton(me){
+            $('.button_' + me).addClass('active');
+            setTimeout(function(){
+              $('.button_' + me).removeClass('active');  
+            }, 500);
+            
+            }
+        
+            
+        
+        
+    
+    
+    $(".testBTN").on('click', function(){
+        getNextColor();
+        $(".gameSeq").text(gameSeq);
+        setTimeout(function() {
+            gameSeq.forEach(displayButton)
+        }, 500);
+    });
+    
+    $('.zero').on('click', function(){
+        playerSeq.push(0);
+    });
+    $('.one').on('click', function(){
+        playerSeq.push(1);
+    })
+    $('.two').on('click', function(){
+        playerSeq.push(2);
+    })
+    $('.three').on('click', function(){
+        playerSeq.push(3);
+    })
+    
     
 })
